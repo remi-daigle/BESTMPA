d97da4f7c941853e967a5158d4e31f1bce090403
Remi Daigle <remi.daigle@dal.ca>
2016-10-11 12:17:09 -0400

    generalize initgrid
    'areas' argument instead of specifying Habitat and Breeding

--- R/initgrid.R
+++ R/initgrid.R
@@ -1,19 +1,26 @@
 #' Title
 #'
 #' @param EEZ
-#' @param Habitats
-#' @param Breeding
 #' @param cell_size
 #' @param proj
+#' @param areas
 #'
 #' @return
 #' @export
 #'
 #' @examples
-initgrid <- function(EEZ,Habitats,Breeding,cell_size,proj){
+initgrid <- function(EEZ,cell_size,proj,areas=NA){
+    # browser()
+    if(is.null(names(areas))) warning("areas are not named (e.g. areas=c(Breeding=Breeding))")
+
     EEZ <- spTransform(EEZ,CRS(proj))
-    Habitats <- spTransform(Habitats,CRS(proj))
-    Breeding <- spTransform(Breeding,CRS(proj))
+    if(!any(is.na(areas))){
+        for(i in seq_along(areas)){
+            areas[[i]] <- spTransform(areas[[i]],CRS(proj))
+        }
+    }
+    # Habitats <- spTransform(Habitats,CRS(proj))
+    # Breeding <- spTransform(Breeding,CRS(proj))
 
     #### create habitat grid ####
     bb <- bbox(EEZ)
@@ -37,8 +44,15 @@ initgrid <- function(EEZ,Habitats,Breeding,cell_size,proj){
     spChFIDs(p) <- paste0("c",1:length(p))
     p@data <- data.frame(cell_ID=paste0("c",1:length(p)))
 
-    #### set habitat and breeding ground overlaps
-    p$Habitats <- apply(gIntersects(Habitats,p,byid=T),1,any)
-    p$Breeding <- apply(gIntersects(Breeding,p,byid=T),1,any)
+    #### set area overlaps
+    if(!any(is.na(areas))){
+        for(i in seq_along(areas)){
+            areas[[i]] <- apply(gIntersects(areas[[i]],p,byid=T),1,any)
+            p@data <- data.frame(p@data,areas[[i]])
+        }
+        names(p@data) <- c("cell_ID",names(areas))
+    }
+    # p$Habitats <- apply(gIntersects(Habitats,p,byid=T),1,any)
+    # p$Breeding <- apply(gIntersects(Breeding,p,byid=T),1,any)
     return(p)
 }
