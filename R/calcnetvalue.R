8780e5958ba8334cb4aaeb0e972537abb211781e
Remi Daigle <remi.daigle@dal.ca>
2016-11-07 12:20:07 -0500

    add documentation for calcnetvalue

--- R/calcnetvalue.R
+++ R/calcnetvalue.R
@@ -1,13 +1,18 @@
-#' Title
+#' Calculate net value
 #'
-#' @param catch_summary
-#' @param Status_quo_profitability
-#' @param fish_operating_cost_ratio
+#' Calculates the net value of the model output (This function cannot work without model output). Uses the \code{Status_quo_profitability} ratio and the weighted mean of the distance travelled to get a fish (weighted by the catch gross value), and the \code{fish_operating_cost_ratio} to determine the relative profitability of scenarios
+#'
+#' @param catch_summary A merged dataframe of all the 'kg' output files
+#' @param Status_quo_profitability The ratio of the landed value of the catch to the total operating costs for the Mixed Fishery
+#' @param fish_operating_cost_ratio The ratio of operating cost that are variable with distance (labour and fuel) to the total operating costs for the Mixed Fishery
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' filenames <- list.files(results_folder,pattern="kg_")
+#' catch_summary <- rbindlist(lapply(filenames,processkg,results_folder,distance,fish_landed_value))
+#' catch_summary <- calcnetvalue(catch_summary,Status_quo_profitability,fish_operating_cost_ratio)
 calcnetvalue <- function(catch_summary,Status_quo_profitability,fish_operating_cost_ratio){
     SQinitialstats <- catch_summary %>%
         filter(scenario=="SQ",year==min(time)) %>%
