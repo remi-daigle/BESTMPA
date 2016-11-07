bcc9e6705551c4bd8245009edfc0b35ef1fd994b
Remi Daigle <remi.daigle@dal.ca>
2016-11-07 16:06:22 -0500

    fix harvest and add documentation

--- R/harvest.R
+++ R/harvest.R
@@ -5,19 +5,37 @@
 #' @param ages
 #' @param distance
 #' @param fish_licenses
-#' @param mpaSQ
-#' @param mpa2020
-#' @param writefish
-#' @param writefishcatch
-#' @param writekg
+#' @param mpabefore Logical vector, TRUE if closed to fishing before \code{time}
+#' @param mpaafter Logical vector, TRUE if closed to fishing during \code{time}
+#' @param writefish If TRUE, will write the \code{fish} remaining for each cell/age to the \code{results_folder}
+#' @param writefishcatch If TRUE, will write the \code{fish} caught for each cell/age to the \code{results_folder}
+#' @param writekg (logical) If TRUE, will write the number of kg of fish caught for each cell/community to the \code{results_folder}
+#' @param results_folder Path to the directory where results are stored
+#' @param y current year
+#' @param time years during which model results are recorded (i.e. not during spin-up time)
+#' @inheritParams fishing
+#' @inheritParams dispersal
 #'
 #' @return
 #' @export
 #'
 #' @examples
-harvest <- function(fish,quota,ages=min_age_catch:maxage,distance,fish_licenses,mpaSQ,mpa2020,writefish=TRUE,writefishcatch=TRUE,writekg=TRUE,results_folder){
-    if(!y %in% time) fishcatch <- fishing(fish,quota,ages,distance,fish_licenses,mpa=mpaSQ)
-    if(y %in% time) fishcatch <- fishing(fish,quota,ages,distance,fish_licenses,mpa=mpa2020)
+#' #' # create a fish matrix
+#' fish <- initpop(initial_abun=250*10^6,cells=length(BESTMPA_domain),maxage=50,rate=0.7)
+#' # calculate distances from shore
+#' distance <- gDistance(spTransform(fish_communities,proj),BESTMPA_domain,byid = T)
+#' # number of licenses per region in fish_communities
+#' fish_licenses <- c(866, 4714, 3002, 879, 963)
+#' # define MPAs
+#' mpa <- sample(c(TRUE,FALSE),length(BESTMPA_domain),replace=TRUE)
+#' nompa <- rep(FALSE,length(BESTMPA_domain))
+#' # quota
+#' quota <- estimatequota(fish,maxage=20,y=2001,tot_time=2001:2071,FMSY=0.28,FMSY_buffer=0.667)
+#' # go fishing!
+#' fish <- harvest(fish,quota,ages=4:20,distance,fish_licenses,mpabefore=nompa,mpaafter=mpa,writefish=FALSE,writefishcatch=FALSE,writekg=TRUE,results_folder='',y=2001,time=time)
+harvest <- function(fish,quota,ages=min_age_catch:maxage,distance,fish_licenses,mpabefore,mpaafter,writefish=TRUE,writefishcatch=TRUE,writekg=TRUE,results_folder,y,time){
+    if(!y %in% time) fishcatch <- fishing(fish,quota,ages,distance,fish_licenses,mpa=mpabefore)
+    if(y %in% time) fishcatch <- fishing(fish,quota,ages,distance,fish_licenses,mpa=mpaafter)
 
 
     catch <- Reduce("+",fishcatch)
