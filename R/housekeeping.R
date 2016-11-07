9d627abd136dcad57ccb70fdb59987ad26fc938f
Remi Daigle <remi.daigle@dal.ca>
2016-11-07 15:27:52 -0500

    add documentation to housekeeping

--- R/housekeeping.R
+++ R/housekeeping.R
@@ -1,16 +1,19 @@
-#' Title
+#' Prepare environment and file structure
 #'
-#' @param env
-#' @param fig
+#' @description Prepares the environment and your file structure for modelling. This function will conveniently create a results folder, clear the environment, figures, delete previous results, and increase the memory limit if desired.
+#'
+#' @param env (logical) If TRUE, clears the environment
+#' @param fig (logical) If TRUE, clears the figures
 #' @param results_folder
-#' @param delete
-#' @param memorylimit
+#' @param delete (logical) If TRUE, deletes the all the files in the results_folder. WARNING: files are permanently deleted, they will not be in your "recycling bin". Only use TRUE if you are certain you want to delete the files
+#' @param memorylimit increases the limit in force on the total memory allocation for Windows machines.
 #'
-#' @return
+#' @return NA
 #' @export
 #'
 #' @examples
-housekeeping <- function(results_folder="output",env=FALSE,fig=FALSE,delete=FALSE,memorylimit=memory.limit()){
+#' housekeeping(results_folder=results_folder,env=TRUE,fig=TRUE,delete=FALSE,memorylimit=16304)
+housekeeping <- function(results_folder="output",env=FALSE,fig=FALSE,delete=FALSE,memorylimit=16304){
 
     if(length(dev.list()["RStudioGD"])>1&fig) dev.off(dev.list()["RStudioGD"])
 
@@ -27,7 +30,9 @@ housekeeping <- function(results_folder="output",env=FALSE,fig=FALSE,delete=FALS
     Sys.sleep(0.1)
     if(!dir.exists(results_folder)) dir.create(results_folder)
 
-    if(memorylimit>memory.limit()) memory.limit(size=memorylimit)
+    if(.Platform$OS.type=="windows"){
+        if(memorylimit>memory.limit()) memory.limit(size=memorylimit)
+    }
 
     tmp <- ls(envir=globalenv())
 
