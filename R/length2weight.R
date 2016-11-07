1f4ceb05e74c0fd2d4a33a2800ede273c65519f2
Remi Daigle <remi.daigle@dal.ca>
2016-11-07 15:29:23 -0500

    add documentation to length2weight

--- R/length2weight.R
+++ R/length2weight.R
@@ -1,13 +1,16 @@
-#' Title
+#' Calculate weight from length
 #'
-#' @param length
-#' @param l_to_w_int
-#' @param l_to_w_power
+#' Calculates weight from lenght using the formula \code{weight <- l_to_w_int * length^l_to_w_power}
+#'
+#' @param length fish length in cm. can be easily calculated using \code{\link{age2length}}
+#' @param l_to_w_int The intercept in the Length-weight relationship
+#' @param l_to_w_power The power in the Length-weight relationship
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' weight <- length2weight(length=50,l_to_w_int=0.000011,l_to_w_power=2.91)
 length2weight <- function(length,l_to_w_int=0.000011,l_to_w_power=2.91){
     l_to_w_int*length^l_to_w_power
 }
